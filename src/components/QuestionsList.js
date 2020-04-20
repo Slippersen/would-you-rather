02dbc59a64d9f202b7eed511e74910d1a0466014
Slippersen <erik.sandal@live.no>
2020-04-20 20:26:57 +0200

    Show answered/unanswered questions

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -4,10 +4,19 @@ import styled from "styled-components";
 import loadingGif from "../img/loading.gif";
 
 const QuestionsListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   background-color: #fafafa;
   border: 1px solid lightgray;
   border-radius: 5px;
   padding: 24px 48px;
+  margin: 24px;
+`;
+
+const QuestionsCategoryContainer = styled.div`
+  display: inline-block;
+  max-width: 40%;
+  margin: 0 5%;
 `;
 
 const StyledHeader = styled.p`
@@ -15,8 +24,8 @@ const StyledHeader = styled.p`
 `;
 
 const StyledList = styled.ul`
-  list-style: none;
-  padding: 0;
+  /* list-style: none; */
+  /* padding: 0; */
 `;
 
 const StyledListEntry = styled.li`
@@ -33,6 +42,7 @@ const StyledLoadingGif = styled.img`
 `;
 
 const QuestionsList = () => {
+  const loggedInUser = useSelector((state) => state.users.loggedInUser);
   const questions = useSelector((state) => state.questions.questions);
 
   if (questions.length === 0) {
@@ -41,12 +51,36 @@ const QuestionsList = () => {
 
   return (
     <QuestionsListContainer>
-      <StyledHeader>Questions:</StyledHeader>
-      <StyledList>
-        {questions?.map((question) => (
-          <StyledListEntry key={question.id}>{question.id}</StyledListEntry>
-        ))}
-      </StyledList>
+      <QuestionsCategoryContainer>
+        <StyledHeader>Unanswered questions</StyledHeader>
+        <StyledList>
+          {questions?.map(
+            (question) =>
+              !question.optionOne.votes.includes(loggedInUser.id) &&
+              !question.optionTwo.votes.includes(loggedInUser.id) && (
+                <StyledListEntry key={question.id}>
+                  {question.optionOne.text} or {question.optionTwo.text} (
+                  {question.author})
+                </StyledListEntry>
+              )
+          )}
+        </StyledList>
+      </QuestionsCategoryContainer>
+      <QuestionsCategoryContainer>
+        <StyledHeader>Answered questions</StyledHeader>
+        <StyledList>
+          {questions?.map(
+            (question) =>
+              (question.optionOne.votes.includes(loggedInUser.id) ||
+                question.optionTwo.votes.includes(loggedInUser.id)) && (
+                <StyledListEntry key={question.id}>
+                  {question.optionOne.text} or {question.optionTwo.text} (
+                  {question.author})
+                </StyledListEntry>
+              )
+          )}
+        </StyledList>
+      </QuestionsCategoryContainer>
     </QuestionsListContainer>
   );
 };
