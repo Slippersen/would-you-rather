2702d47d19b35379a8e91f643f825955ef4dacfe
Slippersen <erik.sandal@live.no>
2020-04-28 17:49:08 +0200

    Remove unused starter code

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import usersReducer from "./usersSlice";
 import questionsReducer from './questionsSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     users: usersReducer,
     questions: questionsReducer
   },
